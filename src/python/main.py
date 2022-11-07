3e7289c60a30b700b3b4d4472a3fb220f330da48
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-11-08 00:12:07 +0800

    init(main.py): e-kuaikuai can be run in cli

---
+++ src/python/main.py
@@ -0,0 +1,9 @@
+import argparse
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--file_name')
+    main()
