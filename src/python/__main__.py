cf2112b7fa5a5159fd0336a729e1fe9f363242d7
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2023-07-30 20:49:16 +0800

    init: build with poetry and publish to pypi
    https://pypi.org/project/kuaikuai/

--- src/python/__main__.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--file_name')
-    main()
